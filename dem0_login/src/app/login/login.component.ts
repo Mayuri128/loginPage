0d28aa45d87b2b0b75f83d5153ff11b648b785bf
mayuri <mayuridixit28.md@gmail.com>
2024-11-15 19:35:34 +0530

    login page UI

---
+++ dem0_login/src/app/login/login.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { 
+
+  }
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      userName : ['',Validators.required],
+      password : ['',Validators.required]
+    })
+  }
+
+  onSubmit(){
+
+  }
+
+}
